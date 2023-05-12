a82242eeaa1baf4aae00152bedf7d33e4c09fd7d
Alex Warton <ajameswaust@gmail.com>
2023-05-12 18:45:59 +1000

    added form

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,8 @@ export default function Home() {
   const [transitionStage, setTransitionStage] = useState('fadeIn')
   const [constrolstate, setControlState] = useState(true)
   const [mutedstate, setMutedState] = useState(1)
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
 
   function setLoad() {
     setTransitionStage('fadeOut')
@@ -32,12 +34,12 @@ export default function Home() {
 
   }
 
-  function changeState(){
+  function changeState() {
     setMutedState(0)
   }
 
   const src =
-  "https://ia801602.us.archive.org/15/items/streetcatpreview-v-2/streetcatpreview%20-%20v2.mp4";
+    "https://ia801602.us.archive.org/15/items/streetcatpreview-v-2/streetcatpreview%20-%20v2.mp4";
 
 
   switch (change) {
@@ -51,23 +53,23 @@ export default function Home() {
             {/* <video id='vid' autoPlay muted={mutedstate}>
               <source src={src} type="video/mp4" />
             </video> */}
-              <iframe className='video'
-                width="100%"
-                height="300px"
-                src={`https://www.youtube.com/embed/6Mhradl9XIs?autoplay=1&controls=0&modestbranding=1&mute=${mutedstate}&playsinline=1`}
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                title="Embedded youtube"
-              />
-              
+            <iframe className='video'
+              width="100%"
+              height="300px"
+              src={`https://www.youtube.com/embed/6Mhradl9XIs?autoplay=1&controls=0&modestbranding=1&mute=${mutedstate}&playsinline=1`}
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              title="Embedded youtube"
+            />
+
             <a onClick={() => changeState()} className="text-second"><button>
               Hear the sound
-              </button></a>
+            </button></a>
 
             <a onClick={() => setLoad()} className="text-introduction font-distres"><button>
               find out more
-              </button></a>
+            </button></a>
 
-            
+
 
             <img src='tim.jpg' width="125px" />
           </div>
@@ -80,20 +82,59 @@ export default function Home() {
             <div className="text-introduction font-bungee">
               INSERT ITEMS HERE ETC ETC
             </div>
+            <form>
+
+
+
+              <div className="input">
+                <div className="label">
+                  Email Address
+                </div>
+                <input
+                  className="inputField"
+                  autoFocus
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="example@gmail.com"
+                  type="email"
+                  value={email}
+                />
+              </div>
+
+              <div className="input">
+                <div className="label">
+                  Name
+                </div>
+                <input
+                  className="inputField"
+                  autoFocus
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Alex"
+                  type="text"
+                  value={name}
+                />
+              </div>
+
+
+              <div className="input">
+                <button className="submit" type="submit" value="Join Us">Join Us</button>
+              </div>
+            </form>
           </div>
         </Layout>
       );
     case 'loading':
       return (
-        <Layout>
-          <div className={`container ${transitionStage}`}>
-            <div className="justify">
-              <div className="progress">
-                <div className="progress-value"></div>
+        <>
+          <div className="justify">
+            <div className={`container ${transitionStage} justify`}>
+              <div className="justify">
+                <div className="progress">
+                  <div className="progress-value"></div>
+                </div>
               </div>
             </div>
           </div>
-        </Layout>
+        </>
       )
   }
 
