8f751deb228085ead22e0d5874ab7e78a8a7bf95
Costa Aspromourgos <n10464174@qut.edu.au>
2023-05-15 14:11:15 +1000

    new form

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Layout from './components/layout'
 import Embed from 'react-embed';
 import { useState } from 'react';
 import '../public/tim.jpg'
+import { useRouter } from 'next/router';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,7 @@ export default function Home() {
   const [mutedstate, setMutedState] = useState(1)
   const [email, setEmail] = useState('')
   const [name, setName] = useState('')
+  const router = useRouter();
 
   function setLoad() {
     setTransitionStage('fadeOut')
@@ -42,8 +44,6 @@ export default function Home() {
     "https://ia801602.us.archive.org/15/items/streetcatpreview-v-2/streetcatpreview%20-%20v2.mp4";
 
 
-  switch (change) {
-    case 'home':
       return (
 
         <Layout >
@@ -65,77 +65,12 @@ export default function Home() {
               Hear the sound
             </button></a>
 
-            <a onClick={() => setLoad()} className="text-introduction font-distres"><button>
+            <a onClick={() => router.push('/FormPage')} className="text-introduction font-distres"><button>
               find out more
             </button></a>
-
-
 
             <img src='tim.jpg' width="125px" />
           </div>
         </Layout>
       );
-    case 'form':
-      return (
-        <Layout>
-          <div className={`container fadeIn`}>
-            <div className="text-introduction font-bungee">
-              INSERT ITEMS HERE ETC ETC
-            </div>
-            <form>
-
-
-
-              <div className="input">
-                <div className="label">
-                  Email Address
-                </div>
-                <input
-                  className="inputField"
-                  autoFocus
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="example@gmail.com"
-                  type="email"
-                  value={email}
-                />
-              </div>
-
-              <div className="input">
-                <div className="label">
-                  Name
-                </div>
-                <input
-                  className="inputField"
-                  autoFocus
-                  onChange={(e) => setName(e.target.value)}
-                  placeholder="Alex"
-                  type="text"
-                  value={name}
-                />
-              </div>
-
-
-              <div className="input">
-                <button className="submit" type="submit" value="Join Us">Join Us</button>
-              </div>
-            </form>
-          </div>
-        </Layout>
-      );
-    case 'loading':
-      return (
-        <>
-          <div className="justify">
-            <div className={`container ${transitionStage} justify`}>
-              <div className="justify">
-                <div className="progress">
-                  <div className="progress-value"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </>
-      )
-  }
-
 }
