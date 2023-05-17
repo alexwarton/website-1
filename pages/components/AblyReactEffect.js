faac8496cb25b418d67611d59e7c2570a49a4e09
Alex Warton <ajameswaust@gmail.com>
2023-05-17 14:19:31 +1000

    test more!!

--- pages/components/AblyReactEffect.js
+++ pages/components/AblyReactEffect.js
@@ -1,25 +1,30 @@
 import Ably from "ably/promises";
 import { useEffect } from 'react'
 
-const ably = new Ably.Realtime.Promise({ authUrl: '/api/token' });
+// // const ably = new Ably.Realtime.Promise({ authUrl: '/api/token' });
 
-export function useChannel(channelName, callbackOnMessage) {
-    const channel = ably.channels.get(channelName);
-
-    const onMount = () => {
-        channel.subscribe(msg => { callbackOnMessage(msg); });
-    }
-
-    const onUnmount = () => {
-        channel.unsubscribe();
-    }
-
-    const useEffectHook = () => {
-        onMount();
-        return () => { onUnmount(); };
-    };
-
-    useEffect(useEffectHook);
-
-    return [channel, ably];
+export default function test(){
+    <>
+    HELLO!</>
 }
+
+// export function useChannel(channelName, callbackOnMessage) {
+//     const channel = ably.channels.get(channelName);
+
+//     const onMount = () => {
+//         channel.subscribe(msg => { callbackOnMessage(msg); });
+//     }
+
+//     const onUnmount = () => {
+//         channel.unsubscribe();
+//     }
+
+//     const useEffectHook = () => {
+//         onMount();
+//         return () => { onUnmount(); };
+//     };
+
+//     useEffect(useEffectHook);
+
+//     return [channel, ably];
+// }
