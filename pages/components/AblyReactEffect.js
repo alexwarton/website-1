6ddc049a17733d9d8ee59a8c0cbacc6237d040e7
Alex Warton <ajameswaust@gmail.com>
2023-05-17 14:22:58 +1000

    COME ON!!!

--- pages/components/AblyReactEffect.js
+++ pages/components/AblyReactEffect.js
@@ -1,30 +1,26 @@
 import Ably from "ably/promises";
 import { useEffect } from 'react'
 
-// // const ably = new Ably.Realtime.Promise({ authUrl: '/api/token' });
+const ably = new Ably.Realtime.Promise({ authUrl: 'https://limit-edition1.vercel.app/api/token' });
 
-export default function test(){
-    <>
-    HELLO!</>
+
+export function useChannel(channelName, callbackOnMessage) {
+    const channel = ably.channels.get(channelName);
+
+    const onMount = () => {
+        channel.subscribe(msg => { callbackOnMessage(msg); });
+    }
+
+    const onUnmount = () => {
+        channel.unsubscribe();
+    }
+
+    const useEffectHook = () => {
+        onMount();
+        return () => { onUnmount(); };
+    };
+
+    useEffect(useEffectHook);
+
+    return [channel, ably];
 }
-
-// export function useChannel(channelName, callbackOnMessage) {
-//     const channel = ably.channels.get(channelName);
-
-//     const onMount = () => {
-//         channel.subscribe(msg => { callbackOnMessage(msg); });
-//     }
-
-//     const onUnmount = () => {
-//         channel.unsubscribe();
-//     }
-
-//     const useEffectHook = () => {
-//         onMount();
-//         return () => { onUnmount(); };
-//     };
-
-//     useEffect(useEffectHook);
-
-//     return [channel, ably];
-// }
