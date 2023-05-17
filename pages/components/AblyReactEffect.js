7c8a21e1c7b02359df028359c1403b3ba83017ee
Alex Warton <ajameswaust@gmail.com>
2023-05-17 14:15:09 +1000

    test

--- pages/components/AblyReactEffect.js
+++ pages/components/AblyReactEffect.js
@@ -1,7 +1,7 @@
 import Ably from "ably/promises";
 import { useEffect } from 'react'
 
-const ably = new Ably.Realtime.Promise({ authUrl: 'api/token' });
+const ably = new Ably.Realtime.Promise({ authUrl: '/api/token' });
 
 export function useChannel(channelName, callbackOnMessage) {
     const channel = ably.channels.get(channelName);
