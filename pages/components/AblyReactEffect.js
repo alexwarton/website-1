c88139972568b5d6af5493813ecb2b6e71553152
Alex Warton <ajameswaust@gmail.com>
2023-05-17 13:46:59 +1000

    test

--- pages/components/AblyReactEffect.js
+++ pages/components/AblyReactEffect.js
@@ -1,7 +1,7 @@
 import Ably from "ably/promises";
 import { useEffect } from 'react'
 
-const ably = new Ably.Realtime({ authUrl: 'api/createTokenRequest' });
+const ably = new Ably.Realtime({ authUrl: 'https://limit-edition1.vercel.app/api/createTokenRequest' });
 
 export function useChannel(channelName, callbackOnMessage) {
     const channel = ably.channels.get(channelName);
