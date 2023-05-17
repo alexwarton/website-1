a2754ca264c18c20acfae340815a476e6888a0b6
Alex Warton <ajameswaust@gmail.com>
2023-05-17 13:59:19 +1000

    test

--- pages/api/createTokenRequest.js
+++ pages/api/createTokenRequest.js
@@ -1,8 +1,9 @@
 import Ably from "ably/promises";
 
 export default async function handler(req, res) {
-    const client = new Ably.Realtime(process.env.ABLY_API_KEY);
-    const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-nextjs-demo' });
-    console.log(tokenRequestData);
-    res.status(200).json(tokenRequestData);
+    // const client = new Ably.Realtime(process.env.ABLY_API_KEY);
+    // const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-nextjs-demo' });
+    // console.log(tokenRequestData);
+    // res.status(200).json(tokenRequestData);
+    res.status(200).json({ name: "TEST "})
 };
