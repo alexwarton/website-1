6a70ca9a55a320abfd4061e465fb978223a12040
Alex Warton <ajameswaust@gmail.com>
2023-05-17 14:09:58 +1000

    test

--- pages/api/token.js
+++ pages/api/token.js
@@ -3,9 +3,7 @@ import Ably from "ably/promises";
 export default async function handler(req, res) {
     const client = new Ably.Realtime(process.env.ABLY_API_KEY);
     const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-nextjs-demo' });
-    console.log(tokenRequestData);
     res.status(200).json(tokenRequestData);
-    res.status(200).json({ name: "TEST "})
 };
 
 // import type { NextApiRequest, NextApiResponse } from 'next'
