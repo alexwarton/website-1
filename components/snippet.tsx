1a918fb69f6876184af78cabe917ca10d4e07aed
Alex Warton <ajameswaust@gmail.com>
2023-05-19 21:15:59 +1000

    add snippet functionality

---
+++ components/snippet.tsx
@@ -0,0 +1,22 @@
+import { SocialIcon } from 'react-social-icons';
+import ReactAudioPlayer from 'react-audio-player';
+//...
+
+export default function Snippet(src: any) {
+    return (
+        <div className="container fadeIn justify">
+            <ReactAudioPlayer
+
+                src={src.src}
+                autoPlay
+                controls
+
+            />
+            <a href={src.src} download="snip1" target='_blank'>
+                <button className="text-introduction">download</button>
+            </a>
+
+            <SocialIcon url="https://discord.com" fgColor='white'/>
+        </div>
+    )
+}
